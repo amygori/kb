846e897790f08bcf44814acda5daf982ddabb000
Clinton Dreisbach <clinton@dreisbach.us>
2018-05-09 15:07:13 -0400

    Make NumberField a class

--- w3--frontend/examples/simple-form/script.js
+++ w3--frontend/examples/simple-form/script.js
@@ -1,33 +1,53 @@
-var numberField = document.getElementById('my-number')
+class NumberField {
+  constructor (input) {
+    this.input = input
+  }
 
-function markValid (numberField) {
-  numberField.parentElement.classList.add('input-valid')
-  numberField.parentElement.classList.remove('input-invalid')
+  markValid () {
+    this.input.parentElement.classList.add('input-valid')
+    this.input.parentElement.classList.remove('input-invalid')
+  }
+
+  markInvalid (message) {
+    this.input.parentElement.classList.remove('input-valid')
+    this.input.parentElement.classList.add('input-invalid')
+
+    var errorMsg = document.createElement('div')
+    errorMsg.classList.add('input-hint', 'text-danger')
+    errorMsg.innerText = message
+    this.input.parentElement.appendChild(errorMsg)
+  }
+
+  clearErrorMsgs () {
+    this.input.parentElement.querySelectorAll('.input-hint').forEach(function (el) {
+      el.remove()
+    })
+  }
+
+  onChange (listenerFn) {
+    this.input.addEventListener('input', listenerFn)
+  }
+
+  getValue () {
+    return parseInt(this.input.value, 10)
+  }
 }
 
-function markInvalid (numberField, message) {
-  numberField.parentElement.classList.remove('input-valid')
-  numberField.parentElement.classList.add('input-invalid')
+var numberInput = document.getElementById('my-number')
+var numberField = new NumberField(numberInput)
 
-  var errorMsg = document.createElement('div')
-  errorMsg.classList.add('input-hint', 'text-danger')
-  errorMsg.innerText = message
-  numberField.parentElement.appendChild(errorMsg)
-}
-
-numberField.addEventListener('input', function () {
-  numberField.parentElement.querySelectorAll('.input-hint').forEach(function (el) {
-    el.remove()
-  })
+numberField.onChange(function () {
+  numberField.clearErrorMsgs()
 
   // get value of numberField
-  var number = parseInt(numberField.value, 10)
-  // check to see if numberField is >= 1 && <= 10
+  var number = numberField.getValue()
+
+  // check to see if numberField.input is >= 1 && <= 10
   if (number < 1) {
-    markInvalid(numberField, 'Your number is less than 1')
+    numberField.markInvalid('Your number is less than 1')
   } else if (number > 10) {
-    markInvalid(numberField, 'Your number is greater than 10')
+    numberField.markInvalid('Your number is greater than 10')
   } else {
-    markValid(numberField)
+    numberField.markValid()
   }
 })
