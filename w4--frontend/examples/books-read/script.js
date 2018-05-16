31e3b955aa4b2990ede810bfb11f21873d0fec19
Clinton Dreisbach <clinton@dreisbach.us>
2018-05-16 11:00:46 -0400

    [booksdb] Add new book

--- w4--frontend/examples/books-read/script.js
+++ w4--frontend/examples/books-read/script.js
@@ -1,3 +1,5 @@
+import request from 'superagent'
+
 function id (idString) {
   return document.getElementById(idString)
 }
@@ -9,6 +11,26 @@ id('add-book-button').addEventListener('click', function () {
 
 id('add-book-form').addEventListener('submit', function (event) {
   event.preventDefault()
-  id('add-book-form').classList.add('hidden')
-  id('add-book-button').classList.remove('hidden')
+
+  const title = id('title').value
+  const authors = id('authors').value.split(/\s*,\s*/)
+  const status = id('status').value
+
+  request
+    .post('https://books-api.glitch.me/api/books')
+    .auth('clinton', 'password123')
+    .send({
+      title: title,
+      authors: authors,
+      status: status
+    })
+    .then(response => {
+      console.log(response)
+      id('add-book-form').reset()
+      id('add-book-form').classList.add('hidden')
+      id('add-book-button').classList.remove('hidden')
+    })
+    .catch((err) => {
+      console.error(err)
+    })
 })
