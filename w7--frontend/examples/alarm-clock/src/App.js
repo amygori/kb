1ff8f0210b52280a1decc93a4be0cd3d99af33de
Clinton Dreisbach <clinton@dreisbach.us>
2018-06-06 10:17:14 -0400

    Move Dashboard into its own component

--- w7--frontend/examples/alarm-clock/src/App.js
+++ w7--frontend/examples/alarm-clock/src/App.js
@@ -1,59 +1,25 @@
-/* global localStorage, Audio */
-
 import React, { Component } from 'react'
 import request from 'superagent'
-import { DateTime } from 'luxon'
 import uuid from 'uuid/v4'
 
 import Container from './components/shoelace/Container'
-import Clock from './components/Clock'
-import AddAlarmButton from './components/AddAlarmButton'
-import AlarmSelector from './components/AlarmSelector'
-import Alarm from './components/Alarm'
 import Time from './Time'
-import schoolBell from './audio/school-bell.wav'
-import fireAlarm from './audio/school-fire-alarm.wav'
-import shipBell from './audio/ship-bell.wav'
-import Spinner from './components/Spinner'
 import AlarmForm from './components/AlarmForm'
+import Dashboard from './components/Dashboard'
 
 import './App.css'
-
-const alarms = {
-  schoolBell,
-  fireAlarm,
-  shipBell
-}
 
 class App extends Component {
   constructor () {
     super()
     this.state = {
-      currentTime: null,
       alarms: [],
-      alarmSound: '',
       alarmRinging: false,
       addingAlarm: false
     }
   }
 
   componentDidMount () {
-    const alarmSound = localStorage.alarmSound || 'shipBell'
-
-    setTimeout(() => {
-      this.setState({
-        currentTime: DateTime.local(),
-        alarmSound: alarmSound
-      })
-      this.intervalId = setInterval(() => {
-        this.setState((prevState) => {
-          return ({
-            currentTime: prevState.currentTime.plus({seconds: 1})
-          })
-        })
-      }, 1000)
-    }, 1000)
-
     request.get('http://localhost:8000/alarms')
       .then((res) => {
         return res.body
@@ -70,10 +36,6 @@ class App extends Component {
           alarms: alarms
         })
       })
-  }
-
-  componentWillUnmount () {
-    clearInterval(this.intervalId)
   }
 
   addAlarm = (time, name) => {
@@ -98,69 +60,23 @@ class App extends Component {
     request.delete(`http://localhost:8000/alarms/${id}`).end()
   }
 
-  changeAlarmSound = (event) => {
-    this.setState({
-      alarmSound: event.target.value
-    })
-    localStorage.alarmSound = event.target.value
-  }
-
-  ringAlarm = (onOff) => {
-    if (onOff && !this.state.alarmRinging) {
-      this.setState({alarmRinging: true}, () => {
-        const alarmSoundAudio = new Audio(alarms[this.state.alarmSound])
-        this.lastAlarm = alarmSoundAudio
-        alarmSoundAudio.play()
-      })
-    }
-    if (!onOff) {
-      if (this.lastAlarm) {
-        this.lastAlarm.pause()
-      }
-      this.setState({alarmRinging: false})
-    }
-  }
-
   render () {
     return (
       <div className='App'>
         <Container>
-          <Spinner isLoading={!this.state.currentTime}>
-            <div className='row row-end'>
-              <div className='col-3'>
-                <AlarmSelector
-                  currentAlarmSound={this.state.alarmSound}
-                  changeAlarmSound={this.changeAlarmSound} />
-              </div>
-            </div>
-            <Clock currentTime={this.state.currentTime} />
-            <div className='Buttons'>
-              <AddAlarmButton
-                label='3 secs'
-                seconds={3}
-                currentTime={this.state.currentTime}
-                addAlarm={this.addAlarm} />
-              <button onClick={event => this.setState({addingAlarm: true})}>+ alarm</button>
-            </div>
-            {this.state.addingAlarm &&
-              <AlarmForm onSave={(alarm) => {
-                this.addAlarm(alarm.time, alarm.name)
-                this.setState({addingAlarm: false})
-              }} />
-            }
-            <div className='Alarms'>
-              {this.state.alarms.map(alarm => (
-                <Alarm
-                  key={alarm.id}
-                  id={alarm.id}
-                  time={alarm.time}
-                  name={alarm.name}
-                  ringAlarm={this.ringAlarm}
-                  deleteAlarm={this.deleteAlarm}
-                  currentTime={this.state.currentTime} />
-              ))}
-            </div>
-          </Spinner>
+          {this.state.addingAlarm
+            ? <AlarmForm onSave={(alarm) => {
+              this.addAlarm(alarm.time, alarm.name)
+              this.setState({addingAlarm: false})
+            }} />
+            : <Dashboard
+              alarms={this.state.alarms}
+              addAlarm={this.addAlarm}
+              deleteAlarm={this.deleteAlarm}
+              showAlarmForm={(event) => this.setState({addingAlarm: true})}
+            />
+          }
+
         </Container>
       </div>
     )
---
+++ w7--frontend/examples/alarm-clock/src/components/Dashboard.js
@@ -0,0 +1,130 @@
+/* global localStorage, Audio */
+
+import React from 'react'
+import PropTypes from 'prop-types'
+import { DateTime } from 'luxon'
+
+import Time from '../Time'
+import Spinner from './Spinner'
+import AlarmSelector from './AlarmSelector'
+import Clock from './Clock'
+import AddAlarmButton from './AddAlarmButton'
+import Alarm from './Alarm'
+
+import schoolBell from '../audio/school-bell.wav'
+import fireAlarm from '../audio/school-fire-alarm.wav'
+import shipBell from '../audio/ship-bell.wav'
+
+const alarms = {
+  schoolBell,
+  fireAlarm,
+  shipBell
+}
+
+class Dashboard extends React.Component {
+  constructor () {
+    super()
+    this.state = {
+      currentTime: null,
+      alarmSound: ''
+    }
+  }
+
+  componentDidMount () {
+    const alarmSound = localStorage.alarmSound || 'shipBell'
+
+    this.setState({
+      alarmSound: alarmSound
+    })
+
+    setTimeout(() => {
+      this.setState({
+        currentTime: DateTime.local()
+      })
+      this.intervalId = setInterval(() => {
+        this.setState((prevState) => {
+          return ({
+            currentTime: prevState.currentTime.plus({seconds: 1})
+          })
+        })
+      }, 1000)
+    }, 1000)
+  }
+
+  componentWillUnmount () {
+    clearInterval(this.intervalId)
+  }
+
+  changeAlarmSound = (event) => {
+    this.setState({
+      alarmSound: event.target.value
+    })
+    localStorage.alarmSound = event.target.value
+  }
+
+  ringAlarm = (onOff) => {
+    if (onOff && !this.state.alarmRinging) {
+      this.setState({alarmRinging: true}, () => {
+        const alarmSoundAudio = new Audio(alarms[this.state.alarmSound])
+        this.lastAlarm = alarmSoundAudio
+        alarmSoundAudio.play()
+      })
+    }
+    if (!onOff) {
+      if (this.lastAlarm) {
+        this.lastAlarm.pause()
+      }
+      this.setState({alarmRinging: false})
+    }
+  }
+
+  render () {
+    const { alarms, showAlarmForm, addAlarm, deleteAlarm } = this.props
+
+    return (
+      <Spinner isLoading={!this.state.currentTime}>
+        <div className='row row-end'>
+          <div className='col-3'>
+            <AlarmSelector
+              currentAlarmSound={this.state.alarmSound}
+              changeAlarmSound={this.changeAlarmSound} />
+          </div>
+        </div>
+        <Clock currentTime={this.state.currentTime} />
+        <div className='Buttons'>
+          <AddAlarmButton
+            label='3 secs'
+            seconds={3}
+            currentTime={this.state.currentTime}
+            addAlarm={addAlarm} />
+          <button onClick={showAlarmForm}>+ alarm</button>
+        </div>
+        <div className='Alarms'>
+          {alarms.map(alarm => (
+            <Alarm
+              key={alarm.id}
+              id={alarm.id}
+              time={alarm.time}
+              name={alarm.name}
+              ringAlarm={this.ringAlarm}
+              deleteAlarm={deleteAlarm}
+              currentTime={this.state.currentTime} />
+          ))}
+        </div>
+      </Spinner>
+    )
+  }
+}
+
+Dashboard.propTypes = {
+  alarms: PropTypes.arrayOf(PropTypes.shape({
+    id: PropTypes.string.isRequired,
+    time: PropTypes.instanceOf(Time).isRequired,
+    name: PropTypes.string.isRequired
+  })),
+  showAlarmForm: PropTypes.func,
+  addAlarm: PropTypes.func,
+  deleteAlarm: PropTypes.func
+}
+
+export default Dashboard
